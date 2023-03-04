676c5f84d2d4736928d166cc8d525d1eb3fb4652
numoonchld <raghavendra.saralaya@gmail.com>
2023-03-04 06:42:57 +0530

    expanded view collection functionality

--- components/viewCollections.tsx
+++ components/viewCollections.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { FC, useState, useEffect } from "react"
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import {
@@ -17,13 +18,72 @@ import {
 } from '@metaplex-foundation/js';
 
 interface CollectionPaneProps {
-    nftItem: Metadata<JsonMetadata<string>> | Nft | Sft
+    nftItem: Metadata
 }
 
 const CollectionPane: FC<CollectionPaneProps> = ({ nftItem }) => {
-    console.log(nftItem)
-    return <>
-    </>
+    console.log({ nftItem })
+    console.log('NFT mint address: ', nftItem.mintAddress.toString())
+    const [isSFTixCollectionItem, setIsSFTixCollectionItem] = useState(false)
+    const [tourName, setTourName] = useState('')
+    const [tourSymbol, setTourSymbol] = useState('')
+    const [tourDescription, setTourDescription] = useState('')
+    const [tourImageURL, setTourImageURL] = useState('')
+    const [NFTMintAddress] = useState(nftItem.mintAddress.toString())
+
+    const getURIData = async () => {
+        const URIData = await fetch(nftItem.uri)
+        const URIDataJSON = await URIData.json()
+        console.log({ URIDataJSON })
+        const { description } = URIDataJSON
+
+        console.log({ description })
+
+        if (description.split(" => ")[0] === "SFTix") {
+            setIsSFTixCollectionItem(true)
+            setTourName(URIDataJSON.name)
+            setTourSymbol(URIDataJSON.symbol)
+            setTourDescription(URIDataJSON.description.split(" => ")[1])
+            setTourImageURL(URIDataJSON.image)
+        }
+        else setIsSFTixCollectionItem(false)
+    }
+
+    useEffect(() => {
+        getURIData()
+    }, [])
+
+    console.log(`Token Mint: https://explorer.solana.com/address/${NFTMintAddress}?cluster=devnet`)
+    return isSFTixCollectionItem ? <>
+        <div className="jumbotron p-3">
+            <div className="d-flex flex-row">
+                <img
+                    style={{ objectFit: "cover" }}
+                    className="img-fluid"
+                    src={tourImageURL}
+                    width={175}
+                    height={175}
+                    alt={`${tourSymbol} image`}
+                />
+                <div className="mx-3">
+                    <h1 className="display-4">{tourSymbol}</h1>
+                    <p className="lead">{tourName}</p>
+                    <hr className="my-4" />
+                    <p>{tourDescription}</p>
+                    <p className="lead">
+                        <a
+                            className="btn btn-primary"
+                            href={`https://explorer.solana.com/address/${NFTMintAddress}?cluster=devnet`}
+                            role="button" target={"_blank"}
+                        >
+                            View on Solana Explorer
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </> : <></>
+
 }
 
 const ViewCollections: FC = () => {
@@ -64,18 +124,24 @@ const ViewCollections: FC = () => {
     }, [connection, wallet])
 
     return <>
-        <div className="d-flex justify-content-center my-5">
-            <>
+        <div className="card my-5">
+            <div className="card-header">
+                Tours registered with SFTix
+            </div>
+            <div className="card-body d-flex align-items-center justify-content-start ">
                 {isLoadingNFTs &&
                     <div className="spinner-grow" role="status">
                         <span className="visually-hidden">Loading...</span>
                     </div>
                 }
                 {allCreatedNFTsByCurrentWallet?.length === 0 && isLoadingNFTs === false
-                    ? <p> No NFTs created by your wallet found! </p>
-                    : allCreatedNFTsByCurrentWallet?.map(nftItem => <CollectionPane nftItem={nftItem} />)
+                    && <p> No NFTs created by your wallet found! </p>
                 }
-            </>
+                {allCreatedNFTsByCurrentWallet?.length !== 0 && isLoadingNFTs === false
+                    // @ts-ignore
+                    && allCreatedNFTsByCurrentWallet?.map(nftItem => <CollectionPane nftItem={nftItem} />)
+                }
+            </div>
         </div>
     </>
 }
