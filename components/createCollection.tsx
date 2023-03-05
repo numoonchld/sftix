5a94cd06c9b7c65ed65a845fc006c32aa9475cfe
Raghavendra Saralaya <17598505+numoonchld@users.noreply.github.com>
2023-03-05 10:21:08 +0530

    create collection component created

--- components/createCollection.tsx
+++ components/createCollection.tsx
@@ -1,16 +1,25 @@
-import { FC } from "react"
-import { Keypair } from "@solana/web3.js";
+import { FC, ChangeEvent, FormEvent, useState } from "react"
+// import { Keypair } from "@solana/web3.js";
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import {
     bundlrStorage,
     keypairIdentity,
     walletAdapterIdentity,
     Metaplex,
+    toMetaplexFileFromBrowser,
     CreatorInput,
     KeypairSigner,
     JsonMetadata,
-    CreateSftInput
+    CreateNftInput,
+    CreateSftInput,
+    MetaplexFile,
+    NftWithToken,
+    Nft,
 } from '@metaplex-foundation/js';
+
+interface Event<T = EventTarget> {
+    target: T;
+}
 
 
 const CreateCollection: FC = () => {
@@ -19,10 +28,12 @@ const CreateCollection: FC = () => {
 
     // const collectionAuthority = Keypair.generate();
     const collectionAuthority = wallet?.adapter!
-    console.log('Create collection | wallet public key: ', collectionAuthority.publicKey?.toBase58())
+    // const collectionAuthority = wallet!
+    // console.log('Create collection | wallet public key: ', collectionAuthority.publicKey?.toBase58())
+
     const metaplex = Metaplex.make(connection)
         .use(
-            walletAdapterIdentity(wallet?.adapter!)
+            walletAdapterIdentity(collectionAuthority)
         )
         .use(
             bundlrStorage(
@@ -34,26 +45,146 @@ const CreateCollection: FC = () => {
             )
         )
 
+    const [tourName, setTourName] = useState('')
+    const [artistName, setArtistName] = useState('')
+    const [tourArtworkFile, setTourArtworkFile] = useState<MetaplexFile>()
+
+    const handleTourArtworkUpload = async (event: Event<HTMLInputElement>) => {
+        setTourArtworkFile(await toMetaplexFileFromBrowser(event.target.files![0]))
+    }
+
+    const handleCreateTourSubmit = async (event: FormEvent) => {
+        event.preventDefault()
+
+        /* create a collection NFT for each tour */
+        const collectionName = `${tourName} - ${artistName}`
+        const collectionSymbol = `${tourName.split(' ').map(piece => piece[0]).join('')}${artistName.split(' ').map(piece => piece[0]).join('')}`
+        const sellerFeeBasisPoints = 100
+
+        // upload image to metaplex storage to obtain URI
+        const tourArtworkImageURI = await metaplex.storage().upload(tourArtworkFile!)
+        console.log("tour artwork uri:", tourArtworkImageURI)
+
+        // metadata for tour's collection-NFT
+        const collectionNFTJSONMetadata: JsonMetadata = {
+            name: collectionName,
+            symbol: collectionSymbol,
+            description: `SFTix => Metadata upload for ${tourName} tour artwork`,
+            image: tourArtworkImageURI,
+        }
+        console.log({ collectionNFTJSONMetadata })
+
+        const { uri, metadata } = await metaplex
+            .nfts()
+            .uploadMetadata(collectionNFTJSONMetadata);
+        console.log('Collection NFT metadata and URI: ', uri, metadata)
+
+        // tour's collection-NFT to hold event-NFTs
+        const createCollectionNFTInput: CreateNftInput = {
+            uri,
+            name: collectionName,
+            symbol: collectionSymbol,
+            description: `SFTix => Metadata upload for ${tourName} tour artwork`,
+            sellerFeeBasisPoints,
+            isCollection: true,
+        }
+
+        const { nft } = await metaplex
+            .nfts()
+            .create(createCollectionNFTInput)
+
+        console.log(
+            `Token Mint: https://explorer.solana.com/address/${nft.address.toString()}?cluster=devnet`
+        )
+        /*
+        {
+            "name": "Studious Crab Collection",
+                "symbol": "CRAB",
+                    "description": "Collection of 9 crabs seeking refuge from overfishing on the blockchain.",
+                        "image": "collection.png",
+                            "attributes": [],
+                                "properties": {
+                "files": [
+                    {
+                        "uri": "collection.png",
+                        "type": "image/png"
+                    }
+                ]
+            }
+        }
+        */
+
+
+    }
+
     return <>
         <div className="card my-5">
             <div className="card-header">
-                Collection NFT
+                Each tour is a Collection-NFT
             </div>
             <div className="card-body">
-                <h5 className="card-title">Create tour </h5>
-                <form>
+                <h5 className="card-title">Create tour</h5>
+                <form onSubmit={handleCreateTourSubmit}>
                     <div className="form-group mb-5">
-                        <label htmlFor="exampleInputEmail1">Tour name</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="" />
-                        <small id="emailHelp" className="form-text text-muted">(Each tour is a Collection NFT)</small>
+                        <label
+                            htmlFor="tourNameTextInput">
+                            Tour name
+                        </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="tourNameTextInput"
+                            aria-describedby="emailHelp"
+                            placeholder=""
+                            value={tourName}
+                            onChange={e => setTourName(e.target.value)}
+                        />
+                        {/* <small
+                            id="emailHelp"
+                            className="form-text text-muted">
+                            (Each tour is a Collection NFT)
+                        </small> */}
+                    </div>
+                    <div className="form-group mb-5">
+                        <label
+                            htmlFor="artistNameTextInput">
+                            Artist name
+                        </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="artistNameTextInput"
+                            aria-describedby="emailHelp"
+                            placeholder=""
+                            value={artistName}
+                            onChange={e => setArtistName(e.target.value)}
+                        />
+                        {/* <small
+                            id="emailHelp"
+                            className="form-text text-muted">
+                            (Each tour is a Collection NFT)
+                        </small> */}
                     </div>
                     <div className="form-group mb-5">
                         <div className="custom-file row">
-                            <label className="custom-file-label" htmlFor="inputGroupFile01">Choose tour artwork</label>
-                            <input type="file" className="custom-file-input float-right mx-auto" id="inputGroupFile01" />
+                            <label
+                                className="custom-file-label"
+                                htmlFor="tourArtworkFileUpload">
+                                Choose tour artwork
+                            </label>
+                            <input
+                                type="file"
+                                className="custom-file-input"
+                                id="tourArtworkFileUpload"
+                                accept=".gif, .jpg, .png,"
+                                onChange={handleTourArtworkUpload}
+                            />
                         </div>
                     </div>
-                    <button type="submit" className="btn btn-primary">Create</button>
+                    <button
+                        type="submit"
+                        className="btn btn-primary"
+                    >Create</button>
                 </form>
             </div>
         </div>
