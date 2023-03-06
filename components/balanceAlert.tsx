46b1e492273483128f362ed9dc82cb9d4465be38
numoonchld <raghavendra.saralaya@gmail.com>
2023-03-06 22:23:52 +0530

    updated balance reload function

--- components/balanceAlert.tsx
+++ components/balanceAlert.tsx
@@ -17,9 +17,25 @@ const BalanceAlert: FC = () => {
 
     }, [connection, publicKey])
 
+    const reloadBalance = async () => {
+        await connection.getBalance(publicKey!).then(
+            data => setBalance(data)
+        )
+    }
+
     return <>
-        <div className="alert alert-secondary my-5" role="alert">
+        <div
+            className="alert alert-secondary my-5 d-flex justify-content-between align-items-center"
+            role="alert"
+        >
             Wallet Balance: {balance / LAMPORTS_PER_SOL} SOL
+            <button
+                type='button'
+                className='btn btn-light'
+                onClick={reloadBalance}
+            >
+                &#8635;
+            </button>
         </div></>
 }
 
